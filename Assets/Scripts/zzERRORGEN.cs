ef6746d1931ccac5ace5c462281175b360cea929
David-Ammon-Student <daama6e@student.oaisd.org>
2026-04-22 13:59:32 -0400

    getting somewhere with the meshes

---
+++ Assets/Scripts/zzERRORGEN.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zzERRORGEN : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 5;
+        print(a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
