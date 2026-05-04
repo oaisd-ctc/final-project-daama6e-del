2b40def1fb9df628e8370cc21fc6e57f5da16223
David-Ammon-Student <daama6e@student.oaisd.org>
2026-05-03 21:33:06 -0400

    created handling light bulb prefab. working on toggle switch

--- Assets/GamePLAY/AudioAskerEvent.cs
+++ Assets/GamePLAY/AudioAskerEvent.cs
@@ -1,12 +1,22 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class AudioAskerEvent : MonoBehaviour
 {
     [SerializeField] private narratorClips clipToPlay;
+    [SerializeField] private float delay = 0f;
 
     void OnEnable()
     {
-        NarratorScript.instance.PlayOneShot(clipToPlay);
+        StartCoroutine(DelayPlay()); //Forgot the tag, why it wont work. StartCorutine is important.
+    }
+
+    IEnumerator DelayPlay()
+    {
+        yield return new WaitForSeconds(delay); //you see its got the variable in it. yeild is used to tag it as a working timer instead of thread.sleep.
+        NarratorScript.instance.PlayOneShot(clipToPlay); 
+
+        //this.gameObject.SetActive(false); //Optional incase for some reason want to have the audio play again.
     }
 }
