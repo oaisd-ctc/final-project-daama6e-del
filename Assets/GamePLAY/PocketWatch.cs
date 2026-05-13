799a699366892d2a6f8739394ab3f2a9690b66d7
David-Ammon-Student <daama6e@student.oaisd.org>
2026-05-12 23:14:07 -0400

    Uploading pocket watch and slider game kit

--- Assets/GamePLAY/PocketWatch.cs
+++ Assets/GamePLAY/PocketWatch.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PocketWatch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("UI Reference")]
+    public GameObject GameSlider;
 
-    // Update is called once per frame
+    public SliderGameLogic sliderGame; //Field to link the game to so it knows what to listen to
+    public bool watchActive = false; //As it says
+    private bool Zoned = false;
+
     void Update()
     {
-        
+        if (watchActive && Zoned && Input.GetKeyDown(KeyCode.T))
+        {
+            bool success = sliderGame.Result();
+            if (success)
+            {
+                SceneManager.LoadScene("TheHall");
+            }
+            else
+            {
+                SceneManager.LoadScene("Wrong");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SafeZone"))
+        {
+            Zoned = true;
+            if (watchActive)
+            {
+                GameSlider.SetActive(true);
+                print("DoorTriggered.");
+            }
+        }
     }
 }
---
+++ Assets/GamePLAY/SliderGameLogic.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI; // If you use a Slider
+
+public class SliderGameLogic : MonoBehaviour
+{
+    public Slider sliderGame;
+    public float speed = 1.5f;
+    private bool movingForward = true;
+
+    void Update()
+    {
+        if (movingForward) sliderGame.value += speed * Time.deltaTime; //slider moves animatedly
+        else sliderGame.value -= speed * Time.deltaTime;
+
+        if (sliderGame.value >= 1 || sliderGame.value <= 0)
+            movingForward = !movingForward;
+    }
+
+    // This is the "Bool Sender"
+    public bool Result()
+    {
+        if (sliderGame.value >= 0.45f && sliderGame.value <= 0.55f) //checks if between margin
+        {
+            return true;
+            print("It triggered");
+        }
+        return false;
+    }
+}
